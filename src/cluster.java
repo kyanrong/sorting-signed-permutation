7a33ce98059d8bdd4902980bacdf4d9544812f6e
Tamana Anna Tharakan <tamana293@gmail.com>
2015-04-14 03:25:20 +0800

    Added all methods. Need to test

--- src/cluster.java
+++ src/cluster.java
@@ -2,18 +2,18 @@ import java.util.*;
 import java.lang.*;
 
 class cluster{
-	private static ArrayList<String> species = new ArrayList<String>();
+	private static ArrayList<String> children = new ArrayList<String>();
 	private static int numSpecies;
 	private static int index;
 
-	public cluster(String speciesIndex, ArrayList<String> list,){
-		index = speciesName;
-		species = list;
-		numSpecies = species.size();
+	public cluster(int speciesIndex, ArrayList<String> list){
+		index = speciesIndex;
+		children = list;
+		numSpecies = children.size();
 	}
 
-	public ArrayList<String> getSpecies(){
-		return species;
+	public ArrayList<String> getChildren(){
+		return children;
 	}
 
 	public int getNumSpecies(){
--- src/phlyo.java
+++ src/phlyo.java
@@ -3,23 +3,46 @@ import java.lang.*;
 
 class phylo{
 	public static ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();  //store the distance matrix
-	ArrayList<Integer> minDistance = new ArrayList<Integer>();   //store the index of minimum distance for each row in the matrix
+	public static ArrayList<IntegerPair> min = new ArrayList<IntegerPair>();   //store the index of minimum distance for each row in the matrix
 	public static HashMap<String,Integer> SpeciesA =new HashMap<String,Integer>();
 	public static HashMap<Integer, String> SpeciesB =new HashMap<Integer,String>();
 	public static ArrayList<cluster> clusters = new ArrayList<cluster>();
+	public static int numNodes;
+	
 	public static void main (String[] args){
+		
 		Scanner sc = new Scanner(System.in);
-		ArrayList<IntegerPair> minValues = getInput(sc);
-		IntegerPair a = getMin(minValues);
-		System.out.println(a.getA());
-		System.out.println(a.getB());
-		System.out.println(a.getDistance());
+		
+		//GET THE INPUT
+		getInput(sc);
+		
+		//WHILE LOOP WILL START FROM HERE
+		
+		//GET THE MIN VALUE
+		IntegerPair a = getMin();
+
+		//GET THE CHILDREN
+		ArrayList<String> childrenList = getChildren(a);
+
+		//MERGE NODE
+		merge(a,childrenList);
+
+		//FIND THE NEXT MIN VALUE STEP 1
+		ArrayList<IntegerPair> distancesMinVal = getDistances(childrenList,a);
+
+		//FIND THE NEXT MIN VALUE STEP 2
+		getMinValues(distancesMinVal);
+
+		//WHILE LOOP WILL END HERE
+
+
+		
 		
 
 	}
 
 	//Method to obtain input
-	private static ArrayList<IntegerPair> getInput(Scanner sc){
+	private static void getInput(Scanner sc){
 		ArrayList<IntegerPair> min = new ArrayList<IntegerPair>();
 		int numNodes = sc.nextInt();
 		String name ="";
@@ -32,7 +55,8 @@ class phylo{
 			name = sc.next();
 			SpeciesA.put(name,i);
 			SpeciesB.put(i,name);
-			cluster clusterNew = new cluster(i,null,0);
+			ArrayList<String> dummy = new ArrayList<String>();
+			cluster clusterNew = new cluster(i,dummy);
 			clusters.add(clusterNew);
 			ArrayList<Integer> row = new ArrayList<Integer>();
 			num=10000000;
@@ -50,53 +74,127 @@ class phylo{
 			matrix.add(row);
 			
 		}
-		return min;
+		
 			
 	}
 	
 	
 	//Method to obtain the minimum pairwise distance from minValues matrix
-	private static IntegerPair getMin(ArrayList<IntegerPair> minValues){
-		IntegerPair  minRow = minValues.get(0);
-		for(int i=1;i<minValues.size();i++){
-			if(minValues.get(i).getDistance()<minRow.getDistance()){
-				minRow = minValues.get(i);
+	private static IntegerPair getMin(){
+		IntegerPair  minRow = min.get(0);
+		for(int i=1;i<min.size();i++){
+			if(min.get(i).getDistance()<minRow.getDistance()){
+				minRow = min.get(i);
 			}
 		}
 		return minRow;
 	}
 
-	//Method to obtain new distance values
-	private static ArrayList<IntegerPair> distances(ArrayList<IntegerPair> minValues, IntegerPair min){
-		int sum = 0;
-		//if both are clusters
-		if(clusters.get(min.getA())>=SpeciesA.size() && clusters.get(min.getB())>=SpeciesA.size()){
-			ArrayList<String> A = clusters.get(min.getA());
-			ArrayList<String> B = clusters.get(min.getB());
-			ArrayList<String> children = mergeChildren(A,B);
-			for(int i=0;i<minValues;i++){
-				
-			}
-
-
-					
-		}
+	//Method to obtain the children of the min Values
+	private static ArrayList<String> getChildren(IntegerPair minV){
 		
+		ArrayList<String> children = new ArrayList<String>();
+		
+		//if both are nested clusters, the children arraylist will contain both cluster
+		if(clusters.get(minV.getA()).getIndex()>=SpeciesA.size() && clusters.get(minV.getB()).getIndex()>=SpeciesA.size()){
+			ArrayList<String> A = clusters.get(minV.getA()).getChildren();
+			ArrayList<String> B = clusters.get(minV.getB()).getChildren();
+			 children = mergeChildren(A,B);			
+		}
+		//if A is a cluster and B is a node 
+		else if(clusters.get(minV.getA()).getIndex()>=SpeciesA.size()){
+			children = clusters.get(minV.getA()).getChildren();
+			children.add(SpeciesB.get(clusters.get(minV.getB()).getIndex()));
+		}
+		//if B is a cluster and A is a node
+		else if(clusters.get(minV.getB()).getIndex()>=SpeciesA.size()){
+			children = clusters.get(minV.getB()).getChildren();
+			children.add(SpeciesB.get(clusters.get(minV.getA()).getIndex()));
+		}
+		return children;
 	}
 
-	//Method to merger 2 ArrayLists
-	private static ArrayLists<String> mergeChildren(ArrayList<String> A, ArrayList<String> B){
+	//Method to merge 2 ArrayLists
+	private static ArrayList<String> mergeChildren(ArrayList<String> A, ArrayList<String> B){
 		ArrayList<String> children = A;
 		for(int i=0;i<B.size();i++){
 			children.add(B.get(i));
 		}
+		return children;
 	}
 
+	//Method to obtain new distances
+	private static ArrayList<IntegerPair> getDistances(ArrayList<String> children,IntegerPair minVal){
+		int sum=0;
+		int count = 0;
+		
+		ArrayList<String> child = new ArrayList<String>();
+		ArrayList<IntegerPair> newDist = new ArrayList<IntegerPair>();
+		for(int i=0;i<clusters.size();i++){
+			if(i!= minVal.getA() && i != minVal.getB()){
+				//case 1: the cluster is a nested cluster
+				if(clusters.get(i).getIndex()>=SpeciesA.size()){
+					child = clusters.get(i).getChildren();
+					for(int x=0;x<children.size();x++){
+						int indexA = SpeciesA.get(children.get(x));
+						for(int y=0;y<children.size();y++){
+						int indexB = SpeciesA.get(child.get(y));
+						 	sum = sum +matrix.get(indexA).get(indexB);
+					}
+					}
+					sum = sum * (1/(children.size() * child.size()));
+				//case 2: the cluster is a single node
+				}else{
+					int indexA = SpeciesA.get(clusters.get(i).getIndex());
+					for(int j=0;j<children.size();j++){
+						int indexB = SpeciesA.get(children.get(j));
+						sum = sum + matrix.get(i).get(j);
+					}
+					sum = sum * (1/(children.size()));
+			}
+			
+				IntegerPair c = new IntegerPair(i,minVal.getA(),sum);
+				newDist.add(c);
+		sum = 0;
+		child.clear();	}
+		}
+	return newDist;
+	}
+	//Method to find and store the minimum values
+	private static void getMinValues(ArrayList<IntegerPair> newDist){
+		ArrayList<IntegerPair> temp = new ArrayList<IntegerPair>();
+		int minTotal = 10000;
+		int index = 0 ;
+		temp.add(new IntegerPair(0,0,0));
+
+		for(int i =0;i<newDist.size();i++){
+			if(minTotal > newDist.get(i).getDistance()){
+				minTotal = newDist.get(i).getDistance();
+				 index = i;
+			}
+			if(min.get(newDist.get(i).getA()).getDistance() > newDist.get(i).getDistance()){
+				IntegerPair x = new IntegerPair(0,i,newDist.get(i).getDistance());
+				temp.add(x);
+
+			}
+		}
+		temp.set(0,new IntegerPair(0,index,minTotal));
+		min.clear();
+		min = temp;
+	}
+	
 	//Method to merge 2 clusters/node
-	private static void merge(IntegerPair min){
-
-
-
+	private static void merge(IntegerPair minV,ArrayList<String> children){
+		cluster newCluster = new cluster(numNodes++,children);
+		ArrayList<cluster> tempCluster = new ArrayList<cluster>();
+		tempCluster.add(newCluster);
+		for(int i=0;i<min.size()-1;i++){
+			if(i!=minV.getA() && i!= minV.getB()){
+				tempCluster.add(clusters.get(i));
+			}
+		}
+		clusters.clear();
+		clusters=tempCluster;
 	}
 
 }
