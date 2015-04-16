bb6685d90f399a1cd794061dd6d0d157bcc4294f
Anna <i_yaping_i@yahoo.de>
2015-04-16 08:01:13 +0200

    P1 final

--- src/P1.java
+++ src/P1.java
@@ -8,15 +8,15 @@ public class P1 {
 	int seq_len;
 	ComponentPair p1, p2;
 	ArrayList<PermutationPair> permutations;			// all the permutations in the file
-	
+
 	public P1() {
 		System.out.println("********************* P1 *********************");
 		System.out.println("Please input 2 sequences: ");
-		
+
 		permutations = new ArrayList<PermutationPair>(); 
-		
+
 		getInput();
-		
+
 		p1 = findComponents(permutations.get(0).getPiArr(), permutations.get(0).getSigmaArr());
 		//p2 = findComponents(permutations.get(1).getPiArr(), permutations.get(1).getSigmaArr());
 		
@@ -24,11 +24,12 @@ public class P1 {
 		Node subtreeRoot = subtree.getRoot();
 		ArrayList<Path> cover = findCover(subtreeRoot);
 		orientComponents(cover, permutations.get(0));
+		Bergerone(permutations.get(0).getPiArr(), permutations.get(0).getSigmaArr(), permutations.get(1).getPiArr(), permutations.get(1).getSigmaArr());
 		findCoverCost(subtree);
 		//constructTree(p2, seq_len);
-		Bergerone(permutations.get(0).getPiArr(), permutations.get(0).getSigmaArr());
+		
 	}
-	
+
 	private void getInput() {
 		Scanner sc = new Scanner(System.in);
 		int i = 4;
@@ -48,14 +49,14 @@ public class P1 {
 		}
 		sc.close();
 	}
-	
+
 	// separate into unsigned elements and their signs
 	private PermutationPair separate(String[] seq, String name) {
 		ArrayList<Integer> pi = new ArrayList<Integer>();
 		ArrayList<Boolean> sigma = new ArrayList<Boolean>();
-		
+
 		pi.add(0); sigma.add(true);
-		
+
 		for(int i=0; i<seq.length; i++) {
 			int num = Integer.parseInt(seq[i]);
 			if(num > 0) {
@@ -67,31 +68,31 @@ public class P1 {
 				sigma.add(false);
 			}
 		}
-				
+
 		pi.add(seq_len-1); sigma.add(true);
-		
+
 		return new PermutationPair(pi, sigma, name);
 	}
-	
+
 	// pi contains the unsigned elements
 	// sigma contains the signs
 	private ComponentPair findComponents(ArrayList<Integer> pi, ArrayList<Boolean> sigma) {
 		// for each index i, at most one component can start at pos i and at most one component can end at pos i	
 		ArrayList<Component> c_start = new ArrayList<Component>();
 		ArrayList<Component> c_end = new ArrayList<Component>();
-		
+
 		for(int i=0; i<pi.size(); i++) {
 			c_start.add(null);
 			c_end.add(null);
 		}
-		
+
 		Stack<Integer> M1 = new Stack<Integer>();
 		Stack<Integer> M2 = new Stack<Integer>();
 		Stack<Integer> S1 = new Stack<Integer>();
 		Stack<Integer> S2 = new Stack<Integer>();
 		int[] M = new int[pi.size()];
 		int[] m = new int[pi.size()];
-		
+
 		// initialization
 		M1.push(pi.size());
 		M2.push(0); 
@@ -99,7 +100,7 @@ public class P1 {
 		S2.push(0);
 		M[0] = pi.size();
 		m[0] = 0;
-		
+
 		// Compute M[i]: the nearest element of pi that precedes pi(i) and is greater than pi(i)
 		for(int i=1; i<pi.size(); i++) {
 			if(pi.get(i-1) > pi.get(i)) {
@@ -112,7 +113,7 @@ public class P1 {
 				}
 			}
 			M[i] = M1.peek();
-			
+
 			// Find direct components
 			int s = S1.peek();
 			while(pi.get(s)>pi.get(i) || M[s]<pi.get(i)) {
@@ -124,7 +125,7 @@ public class P1 {
 				c_start.set(s, new Component(s, i, oriented));
 				c_end.set(i, new Component(s, i, oriented));
 			}
-			
+
 			// Compute m[i]: the nearest element of pi that precedes pi(i) and is smaller than pi(i)
 			if(pi.get(i-1) < pi.get(i)) {
 				M2.push(pi.get(i-1));
@@ -135,7 +136,7 @@ public class P1 {
 				}
 			}
 			m[i] = M2.peek();
-			
+
 			// Find reversed components
 			int t = S2.peek();
 			while((pi.get(t)<pi.get(i)||m[t]>pi.get(i)) && t>0) {
@@ -147,7 +148,7 @@ public class P1 {
 				c_start.set(t, new Component(t, i, oriented));
 				c_end.set(i, new Component(t, i, oriented));
 			}
-			
+
 			// Update stacks
 			if(sigma.get(i)==true) {
 				S1.push(i);
@@ -156,13 +157,13 @@ public class P1 {
 				S2.push(i);
 			}
 		}
-		
+
 		/*System.out.println(M1.size());
 		for(int j=M1.size()-1; j>-1; j--) {
 			System.out.println(M1.get(j));
 		}*/
-		
-		
+
+
 		/*System.out.println("c_start size = " + c_start.size());
 		for(int i=0; i<c_start.size(); i++) {
 			if(c_start.get(i) != null)
@@ -171,7 +172,7 @@ public class P1 {
 				System.out.println(i + ", null");
 		}
 		System.out.println();
-		
+
 		System.out.println("c_end size = " + c_end.size());
 		for(int i=0; i<c_end.size(); i++) {
 			if(c_end.get(i) != null)	
@@ -180,23 +181,23 @@ public class P1 {
 				System.out.println(i + ", null");
 		}
 		System.out.println();*/
-		
+
 		ComponentPair pair = new ComponentPair(c_start, c_end);
-		
+
 		return pair;
 	}
-	
+
 	private Tree constructTree(ComponentPair pair, int n) {
 		ArrayList<Component> c_s = pair.getCStart();
 		ArrayList<Component> c_e = pair.getCEnd();
-		
+
 		Node root = new Node("square");		// root
 		Node p = new Node("round", c_s.get(0));
 		Node q = root;
-		
+
 		root.addChild(p);
 		p.setParent(root);
-		
+
 		for(int i=1; i<n-1; i++) {
 			if(c_s.get(i) != null) {			// if there is a component starting at pos i
 				if(c_e.get(i) == null) {		// if there is no component starting at pos i
@@ -213,15 +214,15 @@ public class P1 {
 				q = p.getParent();
 			}		
 		}
-		
+
 		Tree tree = new Tree(root);
-		
+
 		//System.out.println("Tree size = " + tree.getTreeSize());
 		//tree.printTree(root);
-		
+
 		return generateSubtree(tree);
 	}
-	
+
 	// Generate T': the smallest subtree of T that contains all unoriented components of P
 	// obtained by recursively removing from T all dangling oriented components and square nodes (post-order traversal)
 	// All leaves of T' will be unoriented components, while internal round nodes may still represent oriented components
@@ -233,12 +234,12 @@ public class P1 {
 
 		//System.out.println("Subtree size = " + subtree.getTreeSize());
 		//subtree.printTree(root);
-		
+
 		//System.out.println("Subtree leaves count = " + subtree.getLeavesCount());
-		
+
 		return subtree;		
 	}
-	
+
 	private boolean remove(Node n) {
 		/*System.out.println(n.getType() + " .Its children are:");
 		for(int i=0; i<n.getChildrenSize(); i++) {
@@ -267,7 +268,7 @@ public class P1 {
 				n.getChildren().remove(i);
 			}
 		}
-		
+
 		if(!n.hasChild()) {
 			if(n.getType().equals("square") || n.getComponent().getOrientation()==true) {
 				return false;
@@ -276,10 +277,10 @@ public class P1 {
 		else {
 			return true;
 		}
-		
+
 		return true;
 	}
-	
+
 	// Find out if a component is oriented
 	// unoriented component: has one or more breakpoints, and (p,q) have the same sign
 	// oriented component: otherwise 
@@ -289,7 +290,7 @@ public class P1 {
 		}
 		return true;
 	}
-	
+
 	private boolean hasBreakpoints(int start, int end, ArrayList<Integer> pi, ArrayList<Boolean> sigma) {
 		for(int i=start; i<end-1; i++) {
 			String ss = sigma.get(i) ? "+" : "-";
@@ -302,7 +303,7 @@ public class P1 {
 		}
 		return false;
 	}
-	
+
 	private boolean hasDifferentSigns(int start, int end, ArrayList<Boolean> sigma) {
 		for(int i=start; i<end-1; i++) {
 			if(sigma.get(i) != (sigma.get(i+1))) {
@@ -311,29 +312,29 @@ public class P1 {
 		}
 		return false;
 	}
-	
-	
+
+
 	// input the root node of T'(only has all unoriented components)
 	private ArrayList<Path> findCover(Node n) {
 		ArrayList<Path> cover = new ArrayList<Path>();
 		ArrayList<Component> list = new ArrayList<Component>();
-		
+
 		list = visit(n, cover, list);
 		cover.add(new Path(list.get(0)));
-		
+
 		System.out.println("Cover size = " + cover.size());
 		for(int i=0; i<cover.size(); i++) {
 			if(cover.get(i).getEndComponent() != null) {
 				System.out.println("	" + cover.get(i).getStartComponent().getStart() + "," + cover.get(i).getStartComponent().getEnd() + " to " + cover.get(i).getEndComponent().getStart() + "," + cover.get(i).getEndComponent().getEnd() );
 			}
-			else {
+			/*else {
 				System.out.println("	" + cover.get(i).getStartComponent().getStart()+","+cover.get(i).getStartComponent().getEnd());
-			}
+			}*/
 		}	
-		
+
 		return cover;
 	}
-	
+
 	private ArrayList<Component> visit(Node n, ArrayList<Path> cover, ArrayList<Component> list) {
 		for(int i=0; i<n.getChildrenSize(); i++) {
 			visit(n.getChildren().get(i), cover, list);
@@ -348,11 +349,11 @@ public class P1 {
 			cover.add(new Path(list.get(0), n.getComponent()));
 			list.clear();
 		}
-		
+
 		return list;
 	}
 
-	
+
 	// apply merge/cut on the cover to orient all components
 	private void orientComponents(ArrayList<Path> cover, PermutationPair pp) {
 		for(int i=0; i<cover.size(); i++) {
@@ -370,14 +371,14 @@ public class P1 {
 		int endidx = c.getEnd();
 		ArrayList<Integer> pi = pp.getPiArr();
 		ArrayList<Boolean> sigma = pp.getSigmaArr();
-		
+
 		Collections.reverse(pi.subList(startidx, endidx+1));
 		for(int i=startidx; i<endidx+1; i++) {
 			sigma.set(i, !sigma.get(i));
 		}
 		Collections.reverse(sigma.subList(startidx, endidx+1));
 	}
-	
+
 	// c1 and c2 are in order, so c1's startidx will always be smallest
 	private void merge(Component c1, Component c2, PermutationPair pp) {
 
@@ -385,19 +386,19 @@ public class P1 {
 		int endidx = c2.getEnd();
 		ArrayList<Integer> pi = pp.getPiArr();
 		ArrayList<Boolean> sigma = pp.getSigmaArr();
-		
+
 		Collections.reverse(pi.subList(startidx, endidx+1));
 		for(int i=startidx; i<endidx+1; i++) {
 			sigma.set(i,  !sigma.get(i));
 		}
 		Collections.reverse(sigma.subList(startidx, endidx+1));
-		
+
 	}
-	
+
 	// Calculated using Theorem 3, page 395 of the paper
 	private int findCoverCost(Tree tree) {
 		int leavesCount = tree.getLeavesCount();
-		
+
 		if(!tree.hasShortBranch()) {
 			return leavesCount+1;
 		}
@@ -405,7 +406,6 @@ public class P1 {
 			return leavesCount;
 		}
 	}
-	
 
 	private static int score (PermutationPair pair){
 		int count = 0;
